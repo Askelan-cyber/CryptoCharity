1c31a22e289e4c0d0e4628fb5f22c0b7927e9393
Hector Rosette Jr <hrosette@u.rochester.edu>
2020-11-22 14:20:34 -0600

    Update CryptoCharity_user_menu.py

--- CryptoCharity_user_menu.py
+++ CryptoCharity_user_menu.py
@@ -79,7 +79,7 @@ def view_itemized_donations():
     
     
     for donation in donations_list:
-         pprint(donation['args'])
+        pprint(f"Charity Event ID: {donation['args']['charityEventID']}, Donor Amount: {crypto_charity.wei_to_eth(donation['args']['donorAmount'])}, Donor Name {donation['args']['donorName']}")
  
 def view_charity_event_info():
     charity_event_id = int(input("Please enter the charity event id that you would like information on? "))
@@ -97,7 +97,20 @@ def cancel_charity_event():
     elif confirmation == "n":
         print("Charity Event was not cancelled.")
 
+def view_all_charity_events():
+    all_charity_events = charity_contract.events.charityEventRegistration.createFilter(fromBlock="0x0")
+    all_charity_events_filter = all_charity_events.get_all_entries()
+    
+    charity_event_list = []
+    for charity_event in all_charity_events_filter:
+        charity_event_list.append(crypto_charity.toDict(charity_event))
 
+    for charity_event in charity_event_list:
+        #pprint(charity_event['args'])
+        event_info = crypto_charity.get_charity_event(int(charity_event['args']['charityEventID']))
+        pprint(event_info)
+        print()
+        
 def show_menu(menu, header=""):
     a = 0
     if menu[-1] != "Exit":
@@ -115,46 +128,62 @@ def show_menu(menu, header=""):
             return n, opt
 
 main_menu = ["Donor", "Recipient", "Charity Event Admin"]
-donor_menu = ["View Charity Event Information", "Donate", "View Total Donations", "View Itemized Donations"]
-charity_admin_menu = ["Register Charity Event", "Approve Charity Event", "Add Administrator", "View Charity Event Information", "Cancel Charity Event"]
-recipient_menu = ["View Total Donations", "View Itemized Donations", "View Charity Event Info"]
+donor_menu = ["View All Charity Events", "View Charity Event Information", "Donate", "View Total Donations", "View Itemized Donations"]
+charity_admin_menu = ["View All Charity Events", "Register Charity Event", "Approve Charity Event", "Add Administrator", "View Charity Event Information", "Cancel Charity Event"]
+recipient_menu = ["View All Charity Events", "View Total Donations", "View Itemized Donations", "View Charity Event Info"]
 
 a = 0 
 while a != len(main_menu):
+    
     a,text = show_menu(main_menu, "Main Menu") 
+    
     if a == 1:
+        
         donor_menu_options = 0
+        
         while donor_menu_options != len(donor_menu):
+            
             donor_menu_options, donor_opt =  show_menu(donor_menu, "Donor Menu")
+            
             if donor_menu_options == 1:
-                view_charity_event_info()
+                view_all_charity_events()
                 input("\nPress Enter to Continue")
 
             elif donor_menu_options == 2:
-                donate()
+                view_charity_event_info()
                 input("\nPress Enter to Continue")
 
             elif donor_menu_options == 3:
-                view_total_donations()
+                donate()
                 input("\nPress Enter to Continue")
 
             elif donor_menu_options == 4:
+                view_total_donations()
+                input("\nPress Enter to Continue")
+
+            elif donor_menu_options == 5:
                 view_itemized_donations()
                 input("\nPress Enter to Continue")
         
     elif a == 2:
+        
         recipient_menu_options = 0
         while recipient_menu_options != len(recipient_menu):
+            
             recipient_menu_options, recipient_opt =  show_menu(recipient_menu, "Recipient Menu")
             if recipient_menu_options == 1:
+                view_all_charity_events()
+                input("\nPress Enter to Continue")
+            
+            elif recipient_menu_options == 2:
                 view_total_donations()
                 input("\nPress Enter to Continue")
 
-            elif recipient_menu_options == 2:
+            elif recipient_menu_options == 3:
                 view_itemized_donations()
                 input("\nPress Enter to Continue")
 
-            elif recipient_menu_options == 3:
+            elif recipient_menu_options == 4:
                 view_charity_event_info()
                 input("\nPress Enter to Continue")
 
@@ -162,24 +191,28 @@ while a != len(main_menu):
         charity_admin_menu_options = 0
         while charity_admin_menu_options != len(charity_admin_menu):
             charity_admin_menu_options, charity_admin_opt = show_menu(charity_admin_menu, "Charity Administrator Menu")
+            
             if charity_admin_menu_options == 1:
+                view_all_charity_events()
+                input("\nPress Enter to Continue")
+
+            if charity_admin_menu_options == 2:
                 register_charity_event()
                 input("\nPress Enter to Continue")
 
-            elif charity_admin_menu_options == 2:
+            elif charity_admin_menu_options == 3:
                 update_charity_event_approval()
                 input("\nPress Enter to Continue")
 
-            elif charity_admin_menu_options == 3:
+            elif charity_admin_menu_options == 4:
                 add_admin()
                 input("\nPress Enter to Continue")
 
-            elif charity_admin_menu_options == 4:
+            elif charity_admin_menu_options == 5:
                 view_charity_event_info()
                 input("\nPress Enter to Continue")
 
-            elif charity_admin_menu_options == 5:
+            elif charity_admin_menu_options == 6:
                 cancel_charity_event()
                 input("\nPress Enter to Continue")
-
 
